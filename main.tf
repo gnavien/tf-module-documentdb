15ad9af9ce515940a9d83001bf3a196238e00337
gnavien <gnavien@gmail.com>
2024-10-08 08:18:34 +0530

    update

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ resource "aws_docdb_subnet_group" "default" {
 }
 
 
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "my-docdb-cluster"
-  engine                  = "docdb"
-  master_username         = "foo"
-  master_password         = "mustbeeightchars"
-  backup_retention_period = 5
-  skip_final_snapshot     = true
-}
+#resource "aws_docdb_cluster" "docdb" {
+#  cluster_identifier      = "my-docdb-cluster"
+#  engine                  = "docdb"
+#  master_username         =
+#  master_password         = "mustbeeightchars"
+#  backup_retention_period = 5
+#  skip_final_snapshot     = true
+#}
 
 
 
