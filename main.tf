ce3496569ae7dce325a7feb4131a977cc6f8bfd6
gnavien <gnavien@gmail.com>
2024-10-16 10:15:23 +0530

    update

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_docdb_cluster" "main" {
   master_username        = data.aws_ssm_parameter.username.value
   master_password        = data.aws_ssm_parameter.password.value
   skip_final_snapshot    = true
-  db_subnet_group_name   = aws_docdb_subnet_group.main.name
-  vpc_security_group_ids = aws_docdb_subnet_group.main.id
+  db_subnet_group_name   = [aws_docdb_subnet_group.main.name]
+  vpc_security_group_ids = [ aws_docdb_subnet_group.main.id ]
   kms_key_id             = var.kms_key_arn
   storage_encrypted      = true
   tags                   = merge({ Name = "${var.component}-${var.env}" }, var.tags)
